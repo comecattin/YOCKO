4160d2023f39ba63bbe2b6d82a9426aceae0a9e8
sequoia-dev <75748278+sequoia-dev@users.noreply.github.com>
2021-10-19 19:37:10 +0200

    add gaussian basis reader

--- tools.py
+++ tools.py
@@ -44,10 +44,53 @@ def charge():
                    'N' : 7, 'O' : 8, 'F' : 9, 'Ne' : 10}
     
     return dico_charge
-        
-        
+
+def read_basis(file_name):
+    """
+    read basis gausian file
+    only with STO-3G in this version
+    """
+    file = open(file_name,'r')
     
+    data = []
+    atoms = []
+    exp_coeff_S = []
+    exp_coeff_SP = []
+    
+    for i, line in enumerate(file):
+        
+        data.append(line.replace('*','').replace('D','e').split())
+        
+        if line[0:2] == 'S ':
+            
+            atoms.append(data[i-1][0])
+            
+        if line[0] == '*':
+            if data[i-4][0] == 'S':
+                exp_coeff_S.append(data[i-3:i])
+            
+            if data[i-4][0] == 'SP':
+                exp_coeff_SP.append(data[i-3:i])
+                exp_coeff_S.append(data[i-7:i-4])
+                                
+    file.close()
+        
+    data = data[12:]
+    
+    return data , atoms, np.array(exp_coeff_S).astype(np.float), np.array(exp_coeff_SP).astype(np.float)
+    
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     file_name = 'luminol.xyz'
     N_atoms, atoms , coord = read_xyz(file_name)
     dico_charge = charge()
+    data , atoms, exp_coeff_S, exp_coeff_SP = read_basis('sto-3g.1.gbs')
