ef0a467cd81d7a5c064811009421e4a0d4f5fc80
sequoia-dev <75748278+sequoia-dev@users.noreply.github.com>
2021-10-26 21:24:13 +0200

    Upload

--- tools.py
+++ tools.py
@@ -12,6 +12,7 @@ lecture de fichier .xyz
 """
 
 import numpy as np
+from scipy import random
 
 def read_xyz(file_name):
     """
@@ -44,6 +45,7 @@ def charge():
                    'N' : 7, 'O' : 8, 'F' : 9, 'Ne' : 10}
     
     return dico_charge
+
 
 def read_basis(file_name):
     """
@@ -80,17 +82,33 @@ def read_basis(file_name):
     return data , atoms, np.array(exp_coeff_S).astype(np.float), np.array(exp_coeff_SP).astype(np.float)
     
 
-
-
-
-
-
-
-
-
-
+def pro_gauss(gauss_A, gauss_B):
+	a,center_A=gauss_A
+	b, center_B= gauss_B
+	p= a+b
+	diff=np.linalg.norm( center_A - center_B )**2
+	Norm= (4*a*b/(math.pi**2))*0.75
+	New_prefac= N*exp(-a*b/p*diff)
+	New_center=(a*center_A+b*center_B)/p
+	
+	   
+	   #Input needs to be the parameters (tuple type) of each gaussian function
+	   #(a, center_A) where (1/(sig*sqrt(2pi)))*exp(-(x-mu)**2/(2sig**2)) is gauss_A 
+	   #as a function of x
+	   #a=1/(2sig**2)
+	   #mu=center_A
+	   #Na prefactor factor=(1/(sig*sqrt(2pi))))
+	#Parameters of the new gaussian
+	
+		
+	return (p, diff,New_prefac,New_center)
+	
+def Gauss_overlap(A,B):
+        
+    
 if __name__ == '__main__':
     file_name = 'luminol.xyz'
     N_atoms, atoms , coord = read_xyz(file_name)
     dico_charge = charge()
     data , atoms, exp_coeff_S, exp_coeff_SP = read_basis('sto-3g.1.gbs')
+	
